fac9bf628f5be21a58db3abcfa085c26169ea0d1
LAPTOP-LUCAS\LucasWin <luxingtao1699@live.com>
2020-05-30 07:36:11 +0800

    simple code as only core code

--- demo/FTPGetFile.c
+++ demo/FTPGetFile.c
@@ -1,11 +1,8 @@
 
 #include "FTPGetFile.h"
 
-#define FTP_GET_TASK_STACK_SIZE 4096
-#define FTP_GET_TASK_PRIOTITY   2
-
-char s_DataSendBuf[1024];
-char s_DataRecvBuf[1024];
+static char s_FTPFileGetSendBuf[1024];
+static char s_FTPFileGetRecvBuf[1024];
 
 
 int FTPGetFileCore(void *arg)
@@ -14,21 +11,4 @@ int FTPGetFileCore(void *arg)
 
 
         /** try to recv file from remote ftp server */
-}
-
-
-
-void FTPGetFileTask(void *arg)
-{
-    while(1)
-    {
-        /** get msg from queue */
-        
-        FTPGetFileCore(arg);
-    }
-}
-
-void FTPGetFileInit(void)
-{
-    sys_thread_new("FTPGetThread", FTPGetFileTask, NULL, FTP_GET_TASK_STACK_SIZE, FTP_GET_TASK_PRIOTITY);
 }
